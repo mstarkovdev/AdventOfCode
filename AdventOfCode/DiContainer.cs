384aa0e7d53e32a21e789b5dda7411d216156b31
Max Starkov <mstarkov@insys.io>
2025-01-21 01:55:29 +0600

    Add DI support

---
+++ AdventOfCode/DiContainer.cs
@@ -0,0 +1,31 @@
+﻿using AdventOfCode._2015;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
+using NLog.Extensions.Logging;
+
+namespace AdventOfCode;
+internal static class DiContainer
+{
+    public static void Configure(HostBuilderContext context, IServiceCollection services)
+    {
+        services.AddLogging(context);
+        services.AddSingleton<Day1>();
+    }
+
+    private static IServiceCollection AddLogging(this IServiceCollection services, HostBuilderContext context)
+    {
+        var nLogProviderOptions = new NLogProviderOptions();
+        var nLogConfiguration =
+            (LoggingConfiguration)new NLogLoggingConfiguration(context.Configuration.GetSection("Logging:NLog"));
+        var nLogFactory = new LogFactory(nLogConfiguration);
+        var nLogProvider = new NLogLoggerProvider(nLogProviderOptions, nLogFactory);
+
+        services.AddSingleton<ILoggerFactory>(serviceProvider => new NLogLoggerFactory(nLogProvider));
+
+        return services;
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace AdventOfCode
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AdventOfCode
 {
     internal class Program
     {
+        private static ILogger<Program> _logger;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var hostBuilder = Host.CreateDefaultBuilder(args);
+            hostBuilder
+                .ConfigureAppConfiguration(ConfigureApplication)
+                .ConfigureServices(DiContainer.Configure);
+            var host = hostBuilder.Build();
+
+            _logger = host.Services.GetService<ILogger<Program>>();
+            _logger.LogInformation("Hello, world!");
+
+            host.Run();
+        }
+
+        private static void ConfigureApplication(HostBuilderContext context, IConfigurationBuilder builder)
+        {
+            context.Configuration = builder
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
         }
     }
 }
