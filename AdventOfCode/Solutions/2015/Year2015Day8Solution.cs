01ffc411311c87af3dc8f129a7d03fc8c6df2727
Max Starkov <mstarkov@insys.io>
2025-01-24 17:55:52 +0600

    Add solution for day 8

---
+++ AdventOfCode/Solutions/2015/Year2015Day8Solution.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Solutions._2015;
+internal class Year2015Day8Solution : IDaySolution
+{
+    public string GetFirstPartAnswer(string inputData)
+    {
+        var literalCount = 0;
+        var memoryChCount = 0;
+
+        var lines = inputData.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var line in lines)
+        {
+            var lineLiteralCount = line.Length;
+
+            var unescapedLine = Regex.Unescape(line.Replace("\\\"", "b"));
+            unescapedLine = unescapedLine.Replace("\"", "");
+            var lineMemoryChCount = unescapedLine.Length;
+
+            literalCount += lineLiteralCount;
+            memoryChCount += lineMemoryChCount;
+        }
+
+        return (literalCount - memoryChCount).ToString();
+    }
+
+    public string GetSecondPartAnswer(string inputData)
+    {
+        var literalCount = 0;
+        var memoryChCount = 0;
+
+        var escapedLiterals = new HashSet<char>
+        {
+            '\"',
+            '\\'
+        };
+
+        var lines = inputData.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var line in lines)
+        {
+            var lineLiteralCount = line.Length;
+
+            var escapedChCount = line.Count(escapedLiterals.Contains);
+            var lineMemoryChCount = lineLiteralCount + escapedChCount + 2;
+
+            literalCount += lineLiteralCount;
+            memoryChCount += lineMemoryChCount;
+        }
+
+        return (memoryChCount - literalCount).ToString();
+    }
+}
